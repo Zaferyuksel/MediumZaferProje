a89a659250c78cbbf25834b41e1af7da9eaaa50f
Zaferyuksel <112967603+Zaferyuksel@users.noreply.github.com>
2023-03-21 12:08:58 +0300

    Add files via upload

--- Medium Zafer Proje/MediumZaferProje/appMy/models.py
+++ Medium Zafer Proje/MediumZaferProje/appMy/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(("Kategori"), max_length=50)
+
+
+class Article(models.Model):
+    user = models.ForeignKey(User, verbose_name=("Kullanıcı"), on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name=("Kategori"), on_delete=models.CASCADE)
+    title = models.CharField(("Makale Başlık"), max_length=50)
+    text = models.TextField(("Makale"))
+    image = models.FileField(("Makale Görsel"), upload_to="", max_length=100)
+    date_now = models.DateField(("Tarih"), auto_now_add=True)
+    user_img = models.FileField(("User Foto"), upload_to=None, max_length=100)
+    read_time = models.FloatField(("Okuma Süresi"), default=0)
